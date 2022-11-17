aec0969283e3e4a18fdeda8517f6165ea931a096
Timo <timo.fischer@protonmail.com>
2022-11-17 18:21:40 +0100

    Fineshing GUI elements

--- main.py
+++ main.py
@@ -19,6 +19,39 @@ class Rahmen(Frame):
 class Application(Frame):
     def __init__(self,master=None):
         Frame.__init__(self, master)
+        self.pack()
+        self.nameFrame=Rahmen(master,'Name:')
+        self.heigtFrame=Rahmen(master,'Körpergröße:')
+        self.weightFrame=Rahmen(master,'Gewicht:')
+        self.buttonFrame=Frame(master)
+        self.buttonFrame.pack()
+        self.okButton=Button(
+            self.buttonFrame,text='OK',width=20)
+        self.okButton['command']=self.action
+        self.okButton.pack(side='left')
+        self.cancelButton=Button(
+            self.buttonFrame,text='Abbrechen',width=20,command=root.destroy)
+        self.cancelButton.pack(side='right')
+        self.listbox=Listbox(master)
+        self.listbox.pack(fill=BOTH)
+    def action(self):
+        user=User(
+            self.nameFrame.text.get(),
+            self.heightFrame.text.get())
+        bmirechner=Bmicalculator()
+        bmi=bmirechner.calculate(
+            user.height,
+            self.weightFrame.text.get())
+        bmirechner.add(user.name,bmi)
+        self.listbox.delete(0,END)
+        if user.name in bmirechner.datastorage:
+            bmis=bmirechner.datastorage[user.name]
+            for bmi in bmis:
+                self.listbox.insert(
+                    END,
+                    user.name+':'+
+                    str(bmi)+':'+
+                    bmirechner.evaluate(bmi))
 
 # Close the mainloop for creating window
 
