66d9dde95f17a0bcd2ca13a21f258878c0f52aa4
Timo <timo.fischer@protonmail.com>
2022-11-17 17:59:36 +0100

    Add Window frame and Applicattion class

--- main.py
+++ main.py
@@ -2,3 +2,19 @@
 
 from tkinter import *
 from health.bmi import User, Bmicalculator
+
+# Create class for the outer 
+
+class Rahmen(Frame):
+    def __init__(self,master=None,labeltext=''):
+        Frame.__init__(self, master)
+
+class Application(Frame):
+    def __init__(self,master=None):
+        Frame.__init__(self, master)
+
+# Close the mainloop for creating window
+
+root=Tk()
+app=Application(master=root)
+app.mainloop()
